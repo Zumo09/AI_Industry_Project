805ed2f1f8ca4c5e47a39bd2db16a9b1f5aec9f4
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2022-05-16 18:28:39 +0200

    train_test_split

--- notebooks/deep_fib/data.py
+++ notebooks/deep_fib/data.py
@@ -1,36 +1,36 @@
 from abc import ABC, abstractmethod
-from typing import Tuple
+from typing import List, Tuple
 
 import pandas as pd
 from utils.data import Marconi100Dataset
 
 
 class DeepFIBDataset(Marconi100Dataset, ABC):
-    def __init__(self, num_data_per_sample: int) -> None:
-        super().__init__()
+    def __init__(self, paths: List[str], num_data_per_sample: int) -> None:
+        super().__init__(paths)
         self.n = num_data_per_sample
 
     def __len__(self) -> int:
         return super().__len__() * self.n
 
-    def __getitem__(self, index: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
-        df = super().__getitem__(index // self.n)
-        masked = self._mask(df)
+    def __getitem__(self, index: int) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series]:
+        original, label = super().__getitem__(index // self.n)
+        masked = self._mask(original)
 
-        return df, masked
+        return original, masked, label
 
     @abstractmethod
-    def _mask(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _mask(self, original: pd.DataFrame) -> pd.DataFrame:
         raise NotImplementedError()
 
 
 class PDataset(DeepFIBDataset):
-    def _mask(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _mask(self, original: pd.DataFrame) -> pd.DataFrame:
         """Pointwise Masking"""
-        return super()._mask(df)
+        return super()._mask(original)
 
 
 class SDataset(DeepFIBDataset):
-    def _mask(self, df: pd.DataFrame) -> pd.DataFrame:
+    def _mask(self, original: pd.DataFrame) -> pd.DataFrame:
         """Sequence Masking"""
-        return super()._mask(df)
+        return super()._mask(original)
--- notebooks/utils/data.py
+++ notebooks/utils/data.py
@@ -1,25 +1,45 @@
+from typing import Optional, Tuple, List
 import os
 
 import pandas as pd
-from torch.utils.data import Dataset
 from tqdm import tqdm
+from torch.utils.data import Dataset
+from sklearn.model_selection import train_test_split
 
 DATASET_PATH = os.path.join("..", "data")
 
 
+def get_dataset_paths() -> List[str]:
+    paths = []  # type: List[str]
+    for name in os.listdir(DATASET_PATH):
+        if "gzip" not in name:
+            continue
+        paths.append(os.path.join(DATASET_PATH, name))
+    return paths
+
+
+def get_train_test_split(
+    test_size: float, random_state: Optional[int] = None
+) -> Tuple[List[str], List[str]]:
+    paths = get_dataset_paths()
+    train, test = train_test_split(
+        paths, test_size=test_size, random_state=random_state
+    )
+    return train, test
+
+
 class Marconi100Dataset(Dataset):
-    def __init__(self) -> None:
+    def __init__(self, paths: List[str]) -> None:
         super().__init__()
-        self.data = []
-        for name in tqdm(os.listdir(DATASET_PATH)):
-            if "gzip" not in name:
-                continue
-            path = os.path.join(DATASET_PATH, name)
-            df = pd.read_parquet(path, engine="pyarrow")
-            self.data.append(df)
+        self.data = [
+            pd.read_parquet(name, engine="pyarrow") for name in tqdm(paths)
+        ]
 
     def __len__(self) -> int:
         return len(self.data)
 
-    def __getitem__(self, index: int) -> pd.DataFrame:
-        return self.data[index]
+    def __getitem__(self, index: int) -> Tuple[pd.DataFrame, pd.Series]:
+        df = self.data[index]
+        label = df["New_label"]
+        data = df.drop(["label", "New_label"], axis=1)
+        return data, label
