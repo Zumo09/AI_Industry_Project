7cb2534da8efc58c7cc4cbc36ff45f2868a04be4
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2022-05-16 16:14:57 +0200

    dataset

--- notebooks/utils/data.py
+++ notebooks/utils/data.py
@@ -1,11 +1,28 @@
+from typing import Generator
 import os
-from typing import List
-import pandas as pd
-from tqdm.notebook import tqdm
 
-def read_dataset(dataset_path: str) -> List[pd.DataFrame]:  
-    ret = []
-    for name in tqdm(os.listdir(dataset_path)):
-        path = os.path.join(dataset_path, name)
-        ret.append(pd.read_parquet(path, engine='pyarrow'))
-    return ret
+import pandas as pd
+from torch.utils.data import Dataset
+
+DATASET_PATH = os.path.join("..", "data")
+
+
+class Marconi100Dataset(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+        self.names = []
+        for name in os.listdir(DATASET_PATH):
+            if "gzip" not in name:
+                continue
+            self.names.append(os.path.join(DATASET_PATH, name))
+
+    def __len__(self) -> int:
+        return len(self.names)
+
+    def __getitem__(self, index: int) -> pd.DataFrame:
+        path = self.names[index]
+        return pd.read_parquet(path, engine="pyarrow")
+    
+    def __iter__(self) -> Generator[pd.DataFrame, None, None]:
+        for i in range(len(self)):
+            yield self[i]
