e889dc728ec2841565d3818bc8025886e7213017
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2022-05-16 12:54:34 +0200

    load data

--- notebooks/utils/data.py
+++ notebooks/utils/data.py
@@ -3,11 +3,9 @@ from typing import List
 import pandas as pd
 from tqdm.notebook import tqdm
 
-DATASET_PATH = os.path.join(os.getcwd(), "new_data")
-
-def read_dataset() -> List[pd.DataFrame]:  
+def read_dataset(dataset_path: str) -> List[pd.DataFrame]:  
     ret = []
-    for name in tqdm(os.listdir(DATASET_PATH)):
-        path = os.path.join(DATASET_PATH, name)
+    for name in tqdm(os.listdir(dataset_path)):
+        path = os.path.join(dataset_path, name)
         ret.append(pd.read_parquet(path, engine='pyarrow'))
     return ret
