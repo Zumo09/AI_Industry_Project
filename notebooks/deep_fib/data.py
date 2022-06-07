e330173a68b19ba1ff634938a3785606927f1a34
Simone Giordani <63203939+Zumo09@users.noreply.github.com>
2022-06-07 14:21:17 +0200

    moved dataset

--- notebooks/deep_fib/data.py
+++ notebooks/deep_fib/data.py
@@ -1,9 +1,6 @@
-from typing import Dict, List, Tuple
-
 import torch
 import numpy as np
-from torch.utils.data import Dataset
-from utils.data import Marconi100Dataset, NUM_FEATURES
+from utils.data import NUM_FEATURES
 
 
 def get_masks(horizon: int, n: int) -> torch.Tensor:
@@ -28,50 +25,4 @@ def get_masks(horizon: int, n: int) -> torch.Tensor:
         masks.append(mask)
 
     return torch.stack(masks)
-
-
-def unfolded_indexes(
-    dataset: Marconi100Dataset, horizon: int, stride: int
-) -> List[Tuple[int, Tuple[int, int]]]:
-    """Return a list of tuples containing:
-    - the index of the dataframe in the marconi dataset
-    - the starting and ending indexes of the selected window
-
-    The dataframes with length less than horizon will be discarded
-        TODO: padding? (may be a problem maybe for what regard the padding method...
-        maybe a constant paddig might be raised as an anomaly)
-    """
-    indexes = []
-    for idx in range(len(dataset)):
-        df, _ = dataset[idx]
-        length = len(df)
-        if length < horizon:
-            continue
-        start = 0
-        end = start + horizon
-        while end < length:
-            indexes.append((idx, (start, end)))
-            start += stride
-            end = start + horizon
-        # keep last
-        indexes.append((idx, (length - 1 - horizon, length - 1)))
-
-    return indexes
-
-
-class DeepFIBDataset(Dataset):
-    def __init__(self, dataset: Marconi100Dataset, horizon: int, stride: int) -> None:
-        self.dataset = dataset
-        self.indexes = unfolded_indexes(dataset, horizon, stride)
-
-    def __len__(self) -> int:
-        return len(self.indexes)
-
-    def __getitem__(self, index: int) -> Dict[str, torch.Tensor]:
-        df_idx, (start, end) = self.indexes[index]
-        data, label = self.dataset[df_idx]
-        data_t = torch.tensor(data.to_numpy())[start:end].float()
-        label_t = torch.tensor(label.to_numpy())[start:end].int()
-
-        return {"data": data_t, "label": label_t}
 
--- notebooks/utils/data.py
+++ notebooks/utils/data.py
@@ -20,6 +20,35 @@ def get_dataset_paths(dataset_base_path: str) -> List[str]:
     return paths
 
 
+def unfolded_indexes(
+    dataset: Marconi100Dataset, horizon: int, stride: int
+) -> List[Tuple[int, Tuple[int, int]]]:
+    """Return a list of tuples containing:
+    - the index of the dataframe in the marconi dataset
+    - the starting and ending indexes of the selected window
+
+    The dataframes with length less than horizon will be discarded
+        TODO: padding? (may be a problem maybe for what regard the padding method...
+        maybe a constant paddig might be raised as an anomaly)
+    """
+    indexes = []
+    for idx in range(len(dataset)):
+        df, _ = dataset[idx]
+        length = len(df)
+        if length < horizon:
+            continue
+        start = 0
+        end = start + horizon
+        while end < length:
+            indexes.append((idx, (start, end)))
+            start += stride
+            end = start + horizon
+        # keep last
+        indexes.append((idx, (length - 1 - horizon, length - 1)))
+
+    return indexes
+
+
 class Marconi100Dataset(Dataset):
     def __init__(self, paths: List[str], scaling: Optional[str] = None) -> None:
         super().__init__()
@@ -54,3 +83,20 @@ class Marconi100Dataset(Dataset):
 
     def __getitem__(self, index: int) -> Tuple[pd.DataFrame, pd.Series]:
         return self.data[index]
+
+
+class UnfoldedDataset(Dataset):
+    def __init__(self, dataset: Marconi100Dataset, horizon: int, stride: int) -> None:
+        self.dataset = dataset
+        self.indexes = unfolded_indexes(dataset, horizon, stride)
+
+    def __len__(self) -> int:
+        return len(self.indexes)
+
+    def __getitem__(self, index: int) -> Dict[str, torch.Tensor]:
+        df_idx, (start, end) = self.indexes[index]
+        data, label = self.dataset[df_idx]
+        data_t = torch.tensor(data.to_numpy())[start:end].float()
+        label_t = torch.tensor(label.to_numpy())[start:end].int()
+
+        return {"data": data_t, "label": label_t}
