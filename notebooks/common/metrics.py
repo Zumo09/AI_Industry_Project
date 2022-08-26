95d29a54674564f719e37c9f7243b1fc9fd252f1
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2022-08-26 11:15:35 +0200

    sklearn metrics

--- notebooks/common/metrics.py
+++ notebooks/common/metrics.py
@@ -1,5 +1,11 @@
-from typing import Dict
+import re
+from typing import Dict, List, Tuple
 from torch import Tensor
+import numpy as np
+import torch
+
+from sklearn.metrics import precision_recall_curve
+import matplotlib.pyplot as plt
 
 
 def _safe_divide(num: Tensor, denom: Tensor) -> Tensor:
@@ -35,3 +41,97 @@ def compute_metrics(preds: Tensor, target: Tensor) -> Dict[str, Tensor]:
 
 def true_positive_rate(preds: Tensor, target: Tensor) -> Tensor:
     return compute_metrics(preds, target)["recall"]
+
+
+def evaluate_thresholds(
+    all_errors: Tensor, all_labels: Tensor
+) -> Dict[str, List[float]]:
+    f1 = []
+    precision = []
+    recall = []
+    tp = []
+    fp = []
+    fn = []
+    thresholds = np.arange(all_errors.min(), all_errors.max(), 1e-3)
+    for at in thresholds:
+        labels = torch.tensor(all_errors > at, dtype=torch.int8)
+        metrics = compute_metrics(labels.flatten(), all_labels.flatten())
+        f1.append(metrics["f1"])
+        precision.append(metrics["precision"])
+        recall.append(metrics["recall"])
+        tp.append(metrics["true_positive"])
+        fp.append(metrics["false_positive"])
+        fn.append(metrics["false_negative"])
+
+    return dict(
+        thresholds=list(thresholds),
+        f1=f1,
+        precision=precision,
+        recall=recall,
+        true_positive=tp,
+        false_positive=fp,
+        false_negative=fn,
+    )
+
+
+def plot_threshold_metrics(
+    metrics: Dict[str, List[float]], figsize: Tuple[int, int] = (20, 10)
+) -> None:
+    axes: Tuple[Tuple[plt.Axes, ...], ...]
+    fig, axes = plt.subplots(2, 3, figsize=figsize)  # type: ignore
+    axes[0][0].set_title("F1")
+    axes[0][0].plot(metrics["thresholds"], metrics["f1"])
+    axes[0][0].set_xlabel("thresholds")
+
+    axes[0][1].set_title("Precision")
+    axes[0][1].plot(metrics["thresholds"], metrics["precision"])
+    axes[0][1].set_xlabel("thresholds")
+
+    axes[0][2].set_title("Recall")
+    axes[0][2].plot(metrics["thresholds"], metrics["recall"])
+    axes[0][2].set_xlabel("thresholds")
+
+    axes[1][0].set_title("True Positives")
+    axes[1][0].plot(metrics["thresholds"], metrics["true_positive"])
+    axes[1][0].set_xlabel("thresholds")
+
+    axes[1][1].set_title("False Positives")
+    axes[1][1].plot(metrics["thresholds"], metrics["false_positive"])
+    axes[1][1].set_xlabel("thresholds")
+
+    axes[1][2].set_title("Precision - Recall")
+    axes[1][2].plot(metrics["precision"], metrics["recall"])
+    axes[1][2].set_xlabel("precision")
+    axes[1][2].set_xlabel("recall")
+    plt.show()
+
+
+def plot_precision_recall_curve(
+    errors: Tensor, labels: Tensor, figsize: Tuple[int, int] = (10, 10)
+) -> None:
+    precision, recall, thresholds = precision_recall_curve(
+        labels.flatten(), errors.flatten()
+    )
+    recall = recall[:-1]
+    precision = precision[:-1]
+    axes: Tuple[Tuple[plt.Axes, ...], ...]
+    _, axes = plt.subplots(2, 2, figsize=figsize)  # type: ignore
+    f1 = 2 * precision * recall / (precision + recall + 1e-9)
+    ((pr_ax, rc_ax), (f1_ax, pr_ax)) = axes
+    pr_ax.set_title("Precision")
+    pr_ax.plot(thresholds, precision)
+    pr_ax.set_xlabel("thresholds")
+
+    rc_ax.set_title("Recall")
+    rc_ax.plot(thresholds, recall)
+    rc_ax.set_xlabel("thresholds")
+
+    f1_ax.set_title("F1")
+    f1_ax.plot(thresholds, f1)
+    f1_ax.set_xlabel("thresholds")
+
+    pr_ax.set_title("Precision - Recall")
+    pr_ax.plot(precision, recall)
+    pr_ax.set_xlabel("precision")
+    pr_ax.set_xlabel("recall")
+    plt.show()
