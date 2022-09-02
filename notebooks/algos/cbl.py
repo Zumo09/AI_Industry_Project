3bcea8747f59d7393f385017cfc38f9997ee7923
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2022-09-02 18:55:41 +0200

    asd

--- notebooks/algos/cbl.py
+++ notebooks/algos/cbl.py
@@ -13,7 +13,8 @@ import numpy as np
 
 from common import metrics
 from common.models.modutils import save_model
-from common.models.resnet import ResNet, ResNetFeatures
+from common.models.resnet import ResNetFeatures
+from common.models.deeplab import DeepLabNet
 
 AugmentFN = Callable[[Tensor], Tensor]
 
@@ -26,8 +27,50 @@ def left_to_right_flipping(dim: int = 1) -> AugmentFN:
     return lambda x: x.flip(dim)
 
 
+def crop_and_resize(expantion: float = 2) -> AugmentFN:
+    if expantion < 1:
+        raise ValueError(f"Expantion must be >= 1, got {expantion}")
+
+    def cr(x: Tensor) -> Tensor:
+        x = x.permute(0, 2, 1)
+        in_len = x.shape[2]
+        size = int(expantion * in_len)
+        x = F.interpolate(x, size=size, mode="linear")
+        start_col = torch.randint(0, in_len, (1,)).item()
+        end_col = start_col + in_len
+        x = x[:, :, start_col:end_col]
+        x = x.permute(0, 2, 1)
+        return x
+
+    return cr
+
+
+def random(fn: AugmentFN, prob: float = 0.5) -> AugmentFN:
+    return lambda x: fn(x) if np.random.rand() < prob else x
+
+
 def pipeline(*augmentations: AugmentFN) -> AugmentFN:
     return reduce(lambda g, f: lambda x: f(g(x)), augmentations)
+
+
+class SimContrastiveLoss(torch.nn.Module):
+    def __init__(self, temperature: float) -> None:
+        super().__init__()
+        self.temperature = temperature
+
+    def forward(self, feats: Tensor) -> Tensor:
+        # Calculate cosine similarity
+        cos_sim = F.cosine_similarity(feats[:, None, :], feats[None, :, :], dim=-1)
+        # Mask out cosine similarity to itself
+        self_mask = torch.eye(cos_sim.shape[0], dtype=torch.bool, device=cos_sim.device)
+        cos_sim.masked_fill_(self_mask, -9e15)
+        # Find positive example -> batch_size//2 away from the original example
+        pos_mask = self_mask.roll(shifts=cos_sim.shape[0] // 2, dims=0)
+        # InfoNCE loss
+        cos_sim = cos_sim / self.temperature
+        nll = -cos_sim[pos_mask] + torch.logsumexp(cos_sim, dim=-1)
+        nll = nll.mean()
+        return nll
 
 
 class CBLEngine:
@@ -36,7 +79,8 @@ class CBLEngine:
         model: ResNetFeatures,
         device: Optional[torch.device] = None,
         optimizer: Optional[Optimizer] = None,
-        augmentation: Optional[AugmentFN] = None,
+        aug_1: Optional[AugmentFN] = None,
+        aug_2: Optional[AugmentFN] = None,
         lr_scheduler: Optional[_LRScheduler] = None,
     ):
         self.device = device or torch.device("cpu")
@@ -45,15 +89,11 @@ class CBLEngine:
         self.optimizer = optimizer
         self.lr_scheduler = lr_scheduler
 
-        self.augmentation = augmentation or identity()
+        self.aug_1 = aug_1 or identity()
+        self.aug_2 = aug_2 or identity()
 
         self.loss = lambda x, y: x - y
         self.metrics = []
-
-    def augment(self, data: Tensor) -> Tuple[Tensor, Tensor]:
-        head_1 = self.augmentation(data.clone())
-        head_2 = self.augmentation(data.clone())
-        return head_1, head_2
 
     def train_step(self, batch: Dict[str, Tensor]) -> Dict[str, float]:
         assert self.optimizer is not None, "Optimizer is None. Engine can't train'"
@@ -62,7 +102,8 @@ class CBLEngine:
 
         inputs = batch["data"].to(self.device)
 
-        head_1_in, head_2_in = self.augment(inputs)
+        head_1_in = self.aug_1(inputs)
+        head_2_in = self.aug_2(inputs)
 
         head_1_out = self.model(head_1_in)
         head_2_out = self.model(head_2_in)
--- notebooks/common/metrics.py
+++ notebooks/common/metrics.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn import metrics as _skm
 import matplotlib.pyplot as plt
 
-plt.style.use("ggplot")
+plt.style.use("ggplot")  # type: ignore
 
 
 def _fp_fn_curve(y_true, y_score, pos_label=None, sample_weight=None):
@@ -71,33 +71,57 @@ def _fp_fn_curve(y_true, y_score, pos_label=None, sample_weight=None):
     return (fps[sl][::-1], fns[sl][::-1], thresholds[sl][::-1])
 
 
-def _safe_divide(num: np.ndarray, den: np.ndarray) -> np.ndarray:
-    res = num / den
-    res[np.isnan(res)] = 0
-    return res
-
-
 def errors_curve(
     signals: Tensor, labels: Tensor, tolerance: int
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    signals = signals.cpu().flatten()
+    y_score = signals.cpu().flatten()
+    y_true = fill_tolerance(labels, tolerance).flatten()
 
-    full_labels = labels.cpu().clone()
-    for x, y in (labels == 1).nonzero():
-        for i in range(-tolerance, tolerance + 1):
-            if 0 <= y + i < full_labels.size(1):
-                full_labels[x, y + i] = 1
-    full_labels = full_labels.flatten()
-
-    fp, _, thrs = _fp_fn_curve(full_labels, signals, pos_label=1)
-    _, fn, _ = _fp_fn_curve(1 - full_labels, -1 * signals, pos_label=0)
+    fp, _, thrs = _fp_fn_curve(y_true, y_score, pos_label=1)
+    _, fn, _ = _fp_fn_curve(1 - y_true, y_score, pos_label=0)
 
     return fp, fn, thrs
 
 
+def errors_curve_2(
+    signals: Tensor, labels: Tensor, tolerance: int
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    y_score = signals.cpu().flatten()
+    y_true = fill_tolerance(labels, tolerance).flatten()
+
+    fps, tps, thresholds = _skm._ranking._binary_clf_curve(y_true, y_score, pos_label=1)
+    # _, tps, _ = _skm._ranking._binary_clf_curve(1 - y_true, y_score, pos_label=0)
+
+    fns = tps[-1] - tps
+
+    # start with false positives zero
+    first_ind = (
+        fps.searchsorted(fps[0], side="right") - 1
+        if fps.searchsorted(fps[0], side="right") > 0
+        else None
+    )
+    # stop with false negatives zero
+    last_ind = tps.searchsorted(tps[-1]) + 1
+    sl = slice(first_ind, last_ind)
+
+    # reverse the output such that list of false positives is decreasing
+    return (fps[sl][::-1], fns[sl][::-1], thresholds[sl][::-1])
+
+
+def fill_tolerance(labels: Tensor, tolerance: int) -> Tensor:
+    y_true = labels.cpu().clone()
+    for x, y in (labels == 1).nonzero():
+        for i in range(-tolerance, tolerance + 1):
+            if 0 <= y + i < y_true.size(1):
+                y_true[x, y + i] = 1
+    return y_true
+
+
 def average_precision_score(signals: Tensor, labels: Tensor) -> float:
-    return _skm.average_precision_score(
-        labels.cpu().flatten(), signals.cpu().flatten(), pos_label=0
+    return float(
+        _skm.average_precision_score(
+            labels.cpu().flatten(), signals.cpu().flatten(), pos_label=0
+        )
     )
 
 
@@ -107,6 +131,12 @@ def precision_recall_curve(
     return _skm.precision_recall_curve(
         labels.cpu().flatten(), signals.cpu().flatten(), pos_label=0
     )
+
+
+def _safe_divide(num: np.ndarray, den: np.ndarray) -> np.ndarray:
+    res = num / den
+    res[np.isnan(res)] = 0
+    return res
 
 
 class HPCMetrics:
@@ -218,7 +248,7 @@ def plot_errors_curve(
     thresholds: np.ndarray,
     figsize: Tuple[int, int] = (15, 5),
 ) -> None:
-    axes: Tuple[plt.Axes, ...]
+    # axes: Tuple[plt.Axes, ...]
     _, axes = plt.subplots(1, 3, figsize=figsize)  # type: ignore
     fn_ax, fp_ax, c_ax = axes
     fn_ax.set_title("False Negatives")
@@ -231,8 +261,8 @@ def plot_errors_curve(
 
     c_ax.set_title(f"Det curve")
     c_ax.plot(false_positives, false_negatives)
-    c_ax.set_ylabel("precision")
-    c_ax.set_xlabel("recall")
+    c_ax.set_ylabel("False Negatives")
+    c_ax.set_xlabel("False Positives")
     plt.show()
 
 
@@ -245,7 +275,7 @@ def plot_precision_recall_curve(
     auc_pr = _skm.auc(recall, precision)
     recall = recall[:-1]
     precision = precision[:-1]
-    axes: Tuple[Tuple[plt.Axes, ...], ...]
+    # axes: Tuple[Tuple[plt.Axes, ...], ...]
     _, axes = plt.subplots(2, 2, figsize=figsize)  # type: ignore
     f1 = 2 * precision * recall / (precision + recall + 1e-9)
     ((prec_ax, rec_ax), (f1_ax, prc_ax)) = axes
