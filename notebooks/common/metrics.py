f476b7c005674523df1e59c13327563d9627488a
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2022-09-02 21:19:48 +0200

    brute force

--- notebooks/common/metrics.py
+++ notebooks/common/metrics.py
@@ -9,131 +9,54 @@ import matplotlib.pyplot as plt
 plt.style.use("ggplot")  # type: ignore
 
 
-def _fp_fn_curve(y_true, y_score, pos_label=None, sample_weight=None):
-    """Compute errors for different probability thresholds.
-
-    Parameters
-    ----------
-    y_true : ndarray of shape (n_samples,)
-        True binary labels. If labels are not either {-1, 1} or {0, 1}, then
-        pos_label should be explicitly given.
-
-    y_score : ndarray of shape of (n_samples,)
-        Target scores, can either be probability estimates of the positive
-        class, confidence values, or non-thresholded measure of decisions
-        (as returned by "decision_function" on some classifiers).
-
-    pos_label : int or str, default=None
-        The label of the positive class.
-        When ``pos_label=None``, if `y_true` is in {-1, 1} or {0, 1},
-        ``pos_label`` is set to 1, otherwise an error will be raised.
-
-    sample_weight : array-like of shape (n_samples,), default=None
-        Sample weights.
-
-    Returns
-    -------
-    fp  : ndarray of shape (n_thresholds,)
-        False positive count (FP) such that element i is the false positive
-        count of predictions with score >= thresholds[i].
-
-    fn  : ndarray of shape (n_thresholds,)
-        False negative count (FN) such that element i is the false negative
-        count of predictions with score >= thresholds[i].
-
-    thresholds : ndarray of shape (n_thresholds,)
-        Decreasing score values.
-
-    """
-    if len(np.unique(y_true)) != 2:
-        raise ValueError(
-            "Only one class present in y_true. Detection error "
-            "tradeoff curve is not defined in that case."
-        )
-
-    fps, tps, thresholds = _skm._ranking._binary_clf_curve(
-        y_true, y_score, pos_label=pos_label, sample_weight=sample_weight
-    )
-
-    fns = tps[-1] - tps
-
-    # start with false positives zero
-    first_ind = (
-        fps.searchsorted(fps[0], side="right") - 1
-        if fps.searchsorted(fps[0], side="right") > 0
-        else None
-    )
-    # stop with false negatives zero
-    last_ind = tps.searchsorted(tps[-1]) + 1
-    sl = slice(first_ind, last_ind)
-
-    # reverse the output such that list of false positives is decreasing
-    return (fps[sl][::-1], fns[sl][::-1], thresholds[sl][::-1])
-
-
 def errors_curve(
-    signals: Tensor, labels: Tensor, tolerance: int
+    signals: Tensor, labels: Tensor, tolerance: int, num_thrs: int = 100
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    y_score = signals.cpu().flatten()
-    y_true = fill_tolerance(labels, tolerance).flatten()
+    y_score = signals.cpu()
+    y_true = labels.cpu()
+    y_true_full = fill_tolerance(y_true, tolerance)
 
-    fp, _, thrs = _fp_fn_curve(y_true, y_score, pos_label=1)
-    _, fn, _ = _fp_fn_curve(1 - y_true, y_score, pos_label=0)
+    mn, mx = y_score.min(), y_score.max()
+    step = (mx - mn) / num_thrs
+    thrs = np.arange(mn, mx, step)
 
-    return fp, fn, thrs
+    fps = []
+    fns = []
+    for th in thrs:
+        preds: Tensor = (y_score > th).to(y_true.dtype)
+        preds_full = fill_tolerance(preds, tolerance)
+        fps.append(((1 - y_true_full) * preds).sum().item())
+        fns.append((y_true * (1 - preds_full)).sum().item())
+
+    fps = np.asarray(fps)
+    fns = np.asarray(fns)
+
+    return fps, fns, thrs
 
 
-def errors_curve_2(
-    signals: Tensor, labels: Tensor, tolerance: int
+def precision_recall_f1(
+    fps: np.ndarray, fns: np.ndarray
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    y_score = signals.cpu().flatten()
-    y_true = fill_tolerance(labels, tolerance).flatten()
-
-    fps, tps, thresholds = _skm._ranking._binary_clf_curve(y_true, y_score, pos_label=1)
-    # _, tps, _ = _skm._ranking._binary_clf_curve(1 - y_true, y_score, pos_label=0)
-
-    fns = tps[-1] - tps
-
-    # start with false positives zero
-    first_ind = (
-        fps.searchsorted(fps[0], side="right") - 1
-        if fps.searchsorted(fps[0], side="right") > 0
-        else None
-    )
-    # stop with false negatives zero
-    last_ind = tps.searchsorted(tps[-1]) + 1
-    sl = slice(first_ind, last_ind)
-
-    # reverse the output such that list of false positives is decreasing
-    return (fps[sl][::-1], fns[sl][::-1], thresholds[sl][::-1])
+    tps = fns[-1] - fns
+    precision = _safe_divide(tps, tps + fps)
+    recall = tps / tps[0]
+    f1 = 2 * _safe_divide(precision * recall, precision + recall)
+    return precision, recall, f1
 
 
 def fill_tolerance(labels: Tensor, tolerance: int) -> Tensor:
-    y_true = labels.cpu().clone()
+    full = labels.cpu().clone()
     for x, y in (labels == 1).nonzero():
         for i in range(-tolerance, tolerance + 1):
-            if 0 <= y + i < y_true.size(1):
-                y_true[x, y + i] = 1
-    return y_true
-
-
-def average_precision_score(signals: Tensor, labels: Tensor) -> float:
-    return float(
-        _skm.average_precision_score(
-            labels.cpu().flatten(), signals.cpu().flatten(), pos_label=0
-        )
-    )
-
-
-def precision_recall_curve(
-    signals: Tensor, labels: Tensor
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    return _skm.precision_recall_curve(
-        labels.cpu().flatten(), signals.cpu().flatten(), pos_label=0
-    )
+            if 0 <= y + i < full.size(1):
+                full[x, y + i] = 1
+    return full
 
 
 def _safe_divide(num: np.ndarray, den: np.ndarray) -> np.ndarray:
+    msk = den == 0
+    den[msk] = 1
+    num[msk] = 0
     res = num / den
     res[np.isnan(res)] = 0
     return res
@@ -154,22 +77,26 @@ class HPCMetrics:
         self.fitted = False
 
     def fit(self, signals: Tensor, labels: Tensor) -> HPCMetrics:
-        fp, fn, th = errors_curve(signals, labels, self.tolerance)
-        self.false_positives = fp
-        self.false_negatives = fn
-        self.thresholds = th
+        fps, fns, ths = errors_curve(signals, labels, self.tolerance)
+        self.false_positives = fps
+        self.false_negatives = fns
+        self.thresholds = ths
 
         fpc = self.c_alarm * self.false_positives
         fnc = self.c_missed * self.false_negatives
         self.cost = fpc + fnc
 
-        tp = fn[-1] - fn
-        self.precision = _safe_divide(tp, tp + fp)
-        self.recall = tp / tp[-1]
+        pr, rc, f1 = precision_recall_f1(fps, fns)
+        self.precision = pr
+        self.recall = rc
+        self.f1_score = f1
+        # tp = fn[-1] - fn
+        # self.precision = _safe_divide(tp, tp + fp)
+        # self.recall = tp / tp[-1]
 
-        _pxr = self.precision * self.recall
-        _ppr = self.precision + self.recall
-        self.f1_score = 2 * _safe_divide(_pxr, _ppr)
+        # _pxr = self.precision * self.recall
+        # _ppr = self.precision + self.recall
+        # self.f1_score = 2 * _safe_divide(_pxr, _ppr)
 
         self.fitted = True
         return self
@@ -182,48 +109,172 @@ class HPCMetrics:
         return best_th, best_cost
 
 
-class HPCMetricsThresholds:
-    def __init__(self, c_alarm: float, c_missed: float, tolerance: int) -> None:
-        self.c_alarm = c_alarm
-        self.c_missed = c_missed
-        self.tolerance = tolerance
+# class HPCMetricsThresholds:
+#     def __init__(self, c_alarm: float, c_missed: float, tolerance: int) -> None:
+#         self.c_alarm = c_alarm
+#         self.c_missed = c_missed
+#         self.tolerance = tolerance
 
-        self.false_positives = np.array([])
-        self.false_negatives = np.array([])
-        self.cost = np.array([])
-        self.precision = np.array([])
-        self.recall = np.array([])
-        self.f1_score = np.array([])
-        self.thresholds = np.array([])
-        self.fitted = False
+#         self.false_positives = np.array([])
+#         self.false_negatives = np.array([])
+#         self.cost = np.array([])
+#         self.precision = np.array([])
+#         self.recall = np.array([])
+#         self.f1_score = np.array([])
+#         self.thresholds = np.array([])
+#         self.fitted = False
 
-    def fit(self, signals: Tensor, labels: Tensor) -> HPCMetricsThresholds:
-        fp, fn, th = errors_curve(signals, labels, self.tolerance)
-        self.false_positives = fp
-        self.false_negatives = fn
-        self.thresholds = th
+#     def fit(self, signals: Tensor, labels: Tensor) -> HPCMetricsThresholds:
+#         fp, fn, th = errors_curve(signals, labels, self.tolerance)
+#         self.false_positives = fp
+#         self.false_negatives = fn
+#         self.thresholds = th
 
-        fpc = self.c_alarm * self.false_positives
-        fnc = self.c_missed * self.false_negatives
-        self.cost = fpc + fnc
+#         fpc = self.c_alarm * self.false_positives
+#         fnc = self.c_missed * self.false_negatives
+#         self.cost = fpc + fnc
 
-        tp = fn[-1] - fn
-        self.precision = _safe_divide(tp, tp + fp)
-        self.recall = tp / tp[-1]
+#         tp = fn[-1] - fn
+#         self.precision = _safe_divide(tp, tp + fp)
+#         self.recall = tp / tp[-1]
 
-        _pxr = self.precision * self.recall
-        _ppr = self.precision + self.recall
-        self.f1_score = 2 * _safe_divide(_pxr, _ppr)
+#         _pxr = self.precision * self.recall
+#         _ppr = self.precision + self.recall
+#         self.f1_score = 2 * _safe_divide(_pxr, _ppr)
 
-        self.fitted = True
-        return self
+#         self.fitted = True
+#         return self
 
-    def optimize(self) -> Tuple[float, float]:
-        assert self.fitted, "Call .fit(...) first"
-        cost = self.cost
-        best_th = self.thresholds[np.argmin(cost)]
-        best_cost = np.min(cost)
-        return best_th, best_cost
+#     def optimize(self) -> Tuple[float, float]:
+#         assert self.fitted, "Call .fit(...) first"
+#         cost = self.cost
+#         best_th = self.thresholds[np.argmin(cost)]
+#         best_cost = np.min(cost)
+#         return best_th, best_cost
+
+# def _fp_fn_curve(y_true, y_score, pos_label=None, sample_weight=None):
+#     """Compute errors for different probability thresholds.
+
+#     Parameters
+#     ----------
+#     y_true : ndarray of shape (n_samples,)
+#         True binary labels. If labels are not either {-1, 1} or {0, 1}, then
+#         pos_label should be explicitly given.
+
+#     y_score : ndarray of shape of (n_samples,)
+#         Target scores, can either be probability estimates of the positive
+#         class, confidence values, or non-thresholded measure of decisions
+#         (as returned by "decision_function" on some classifiers).
+
+#     pos_label : int or str, default=None
+#         The label of the positive class.
+#         When ``pos_label=None``, if `y_true` is in {-1, 1} or {0, 1},
+#         ``pos_label`` is set to 1, otherwise an error will be raised.
+
+#     sample_weight : array-like of shape (n_samples,), default=None
+#         Sample weights.
+
+#     Returns
+#     -------
+#     fp  : ndarray of shape (n_thresholds,)
+#         False positive count (FP) such that element i is the false positive
+#         count of predictions with score >= thresholds[i].
+
+#     fn  : ndarray of shape (n_thresholds,)
+#         False negative count (FN) such that element i is the false negative
+#         count of predictions with score >= thresholds[i].
+
+#     thresholds : ndarray of shape (n_thresholds,)
+#         Decreasing score values.
+
+#     """
+#     if len(np.unique(y_true)) != 2:
+#         raise ValueError(
+#             "Only one class present in y_true. Detection error "
+#             "tradeoff curve is not defined in that case."
+#         )
+
+#     fps, tps, thresholds = _skm._ranking._binary_clf_curve(
+#         y_true, y_score, pos_label=pos_label, sample_weight=sample_weight
+#     )
+
+#     fns = tps[-1] - tps
+
+#     # start with false positives zero
+#     first_ind = (
+#         fps.searchsorted(fps[0], side="right") - 1
+#         if fps.searchsorted(fps[0], side="right") > 0
+#         else None
+#     )
+#     # stop with false negatives zero
+#     last_ind = tps.searchsorted(tps[-1]) + 1
+#     sl = slice(first_ind, last_ind)
+
+#     # reverse the output such that list of false positives is decreasing
+#     return (fps[sl][::-1], fns[sl][::-1], thresholds[sl][::-1])
+
+
+# def errors_curve(
+#     signals: Tensor, labels: Tensor, tolerance: int
+# ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+#     y_score = signals.cpu()
+#     y_true = labels.cpu()
+
+#     fps, _, thrs = _fp_fn_curve(
+#         fill_tolerance(y_true, tolerance).flatten(), y_score.flatten()
+#     )
+#     # _, fn, _ = _fp_fn_curve(1 - y_true, y_score, pos_label=0)
+#     fns = []
+#     for th in thrs:
+#         predictions = fill_tolerance((y_score > th).to(y_true.dtype), tolerance)
+#         fn = (labels * (1 - predictions)).sum().item()
+#         fns.append(fn)
+
+#     fns = np.asarray(fns)
+
+#     return fps, fns, thrs
+
+
+# def errors_curve_2(
+#     signals: Tensor, labels: Tensor, tolerance: int
+# ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+#     y_score = signals.cpu().flatten()
+#     y_true = labels.flatten()
+#     y_true_full = fill_tolerance(labels, tolerance).flatten()
+
+#     fps, _, thresholds = _skm._ranking._binary_clf_curve(y_true_full, y_score)
+#     _, tps, _ = _skm._ranking._binary_clf_curve(y_true, y_score)
+
+#     fns = tps[-1] - tps
+
+#     # start with false positives zero
+#     first_ind = (
+#         fps.searchsorted(fps[0], side="right") - 1
+#         if fps.searchsorted(fps[0], side="right") > 0
+#         else None
+#     )
+#     # stop with false negatives zero
+#     last_ind = tps.searchsorted(tps[-1]) + 1
+#     sl = slice(first_ind, last_ind)
+
+#     # reverse the output such that list of false positives is decreasing
+#     return (fps[sl][::-1], fns[sl][::-1], thresholds[sl][::-1])
+
+
+# def average_precision_score(signals: Tensor, labels: Tensor) -> float:
+#     return float(
+#         _skm.average_precision_score(
+#             labels.cpu().flatten(), signals.cpu().flatten(), pos_label=0
+#         )
+#     )
+
+
+# def precision_recall_curve(
+#     signals: Tensor, labels: Tensor
+# ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+#     return _skm.precision_recall_curve(
+#         labels.cpu().flatten(), signals.cpu().flatten(), pos_label=0
+#     )
 
 
 ###################################################
@@ -231,7 +282,7 @@ class HPCMetricsThresholds:
 ###################################################
 
 
-def plot_cost(cmodel: HPCMetricsThresholds, figsize: Tuple[int, int] = (15, 5)) -> None:
+def plot_cost(cmodel: HPCMetrics, figsize: Tuple[int, int] = (15, 5)) -> None:
     best_th, best_cost = cmodel.optimize()
     _, ax = plt.subplots(figsize=figsize)
     ax.set_title("Cost")
@@ -266,18 +317,16 @@ def plot_errors_curve(
     plt.show()
 
 
-def plot_precision_recall_curve(
+def plot_precision_recall_f1_curve(
     precision: np.ndarray,
     recall: np.ndarray,
+    f1: np.ndarray,
     thresholds: np.ndarray,
     figsize: Tuple[int, int] = (15, 15),
 ) -> None:
     auc_pr = _skm.auc(recall, precision)
-    recall = recall[:-1]
-    precision = precision[:-1]
     # axes: Tuple[Tuple[plt.Axes, ...], ...]
     _, axes = plt.subplots(2, 2, figsize=figsize)  # type: ignore
-    f1 = 2 * precision * recall / (precision + recall + 1e-9)
     ((prec_ax, rec_ax), (f1_ax, prc_ax)) = axes
     prec_ax.set_title("Precision")
     prec_ax.plot(thresholds, precision)
